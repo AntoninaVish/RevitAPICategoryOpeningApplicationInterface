26d841dd86f796fa802fcbc4bc3f1a894f5ce509
AntoninaVish <vishivtonya@gmail.com>
2023-03-12 14:22:38 +0300

    Добавьте файлы проекта.

---
+++ MainView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RevitAPICategoryOpeningApplicationInterface
+{
+    /// <summary>
+    /// Логика взаимодействия для MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView(ExternalCommandData commandData)// при запуске окна передаем commandData
+        {
+            InitializeComponent();
+            MainViewViewModel vm = new MainViewViewModel(commandData);
+            vm.CloseRequest += (s, e) => this.Close();//при выполнении команды закрываем окно, логика по которой окно будет закрываться самостоятельно
+            DataContext = vm;
+        }
+    }
+}
---
+++ MainViewViewModel.cs
@@ -0,0 +1,87 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using RevitAPICategoryOpeningLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPICategoryOpeningApplicationInterface
+{
+    public class MainViewViewModel
+    {
+
+        private ExternalCommandData _commandData;
+
+        private Document _doc;
+
+        public List<ViewPlan> Views { get; }
+
+        public List<Category> Categories { get; }
+
+        public DelegateCommand HideCommand { get; }
+
+        public DelegateCommand TempHideCommand { get; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            _doc = _commandData.Application.ActiveUIDocument.Document;
+
+            Views = ViewsUtils.GetFloorPlanViews(_doc);
+
+            Categories = CategoryUtils.GetCategories(_doc);
+
+            HideCommand = new DelegateCommand(OnHideCommand);
+
+            TempHideCommand = new DelegateCommand(OnTempHideCommand);
+        }
+
+        private void OnTempHideCommand()
+        {
+            if (SelectedViewPlan == null || SelectedCategory == null) //проверяем выбранный вид и выбранную категорию
+                return;
+
+            using (var ts = new Transaction(_doc, "Save changes"))//логика для временного скрытия данной категории
+            {
+                ts.Start();
+                SelectedViewPlan.HideCategoryTemporary(SelectedCategory.Id);
+
+                ts.Commit();
+            }
+
+            RaiseCloseRequest();
+        }
+
+        private void OnHideCommand() //на выбраном плате скрываем выбранную категорию
+        {
+            if (SelectedViewPlan == null || SelectedCategory == null)
+                return;
+
+
+            using (var ts = new Transaction(_doc, "Save changes"))
+            {
+                ts.Start();
+                SelectedViewPlan.SetCategoryHidden(SelectedCategory.Id, hide: true); //вносим изменения, которые хотим увидеть в моделе
+
+                ts.Commit();
+            }
+
+            RaiseCloseRequest();
+        }
+
+        public ViewPlan SelectedViewPlan { get; set; }
+
+        public  Category SelectedCategory { get; set; }
+
+        public event EventHandler CloseRequest;
+
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+}
